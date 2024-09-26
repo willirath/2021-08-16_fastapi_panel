5b8a64f4850161262819b262e11f67a303b578a2
Willi Rath <wrath@geomar.de>
2024-09-26 16:00:12 +0200

    Add second param

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -4,9 +4,9 @@ import pandas as pd
 pn.extension()
 
 
-def get_pi(n=10):
+def get_pi(n="10", digits=3):
     pi = pd.read_json(f"http://compute/pi/?length={n}", typ="series")["pi"]
-    return float(pi)
+    return f"{float(pi):.{digits}f}"
 
 
 bootstrap = pn.template.BootstrapTemplate(title="Estimate PI")
@@ -14,10 +14,7 @@ bootstrap = pn.template.BootstrapTemplate(title="Estimate PI")
 bootstrap.main.append(
     pn.Row(
         pn.Card(
-            pn.interact(
-                get_pi,
-                n=[1, 10, 100, 1_000, 10_000],
-            ),
+            pn.interact(get_pi, n="10", digits=3),
             title="PI from an n rand points",
         ),
     )
